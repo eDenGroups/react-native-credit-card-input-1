7167257c4c9476b32e0763bf52bc76816222a419
Arunram Kalaiselvan <arun@arunram.com>
2020-12-06 02:09:26 -0500

    trying a new keyboard hack

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -250,7 +250,7 @@ const POSTAL_CODE_INPUT_WIDTH = 120 // https://github.com/yannickcr/eslint-plugi
       <ScrollView
         ref="Form"
         horizontal
-        keyboardShouldPersistTaps="always"
+        keyboardShouldPersistTaps="handled"
         scrollEnabled={allowScroll}
         showsHorizontalScrollIndicator={false}
         style={[s.form, formStyle]}>
